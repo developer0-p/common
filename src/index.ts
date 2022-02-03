4ac824d082b3d0a85a17dc6fed8deb11b6667063
Pablo <pablo.developer0@gmail.com>
2022-02-03 11:30:01 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-//re-export all staff from errors
+//re-export all staff from errors and middleware
 export * from './errors/bad-request-error'
 export * from './errors/custom-error'
 export * from './errors/database-connectiion-error'
 export * from './errors/not-authorized-error'
 export * from './errors/not-found-error'
 export * from './errors/request-validation-error'
-//re-export all staff from middleware
+
 export * from './middleware/current-user'
 export * from './middleware/error-handler'
 export * from './middleware/require-auth'
