cd64599469f98c9a9eb9baa23c997fba9d94aeec
Pablo <pablo.developer0@gmail.com>
2022-02-14 08:54:59 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,6 @@ export * from './middleware/validate-request'
 
 export * from './events/base-listener'
 export * from './events/base-publisher'
+export * from './events/subjects'
 export * from './events/ticket-created-event'
 export * from './events/ticket-updated-event'
